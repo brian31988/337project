f9d853d05643739ddc620680ee74d3d40d146131
Brian Marshall <Brian31988@gmail.com>
2016-04-05 17:25:23 -0400

    made some changes to search.php

--- search.php
+++ search.php
@@ -12,7 +12,7 @@
   <li role="presentation"><a href="index.html">Home</a></li>
   <li role="presentation"><a href="aboutus.html">About Us</a></li>
   <li role="presentation"><a href="contactus.html">Contact Us</a></li>
-  <li role="presentation"class="active"><a href="search.html">Search</a></li>
+  <li role="presentation"class="active"><a href="search.php">Search</a></li>
   <li role="presentation"><a href="login.php">Login</a></li>
 <li role="presentation"><a href="register.php">Register</a></li></ul>
 <br>
@@ -31,9 +31,6 @@
 
 
 
-
-
-
 <div class="container">
      <div class="row">
   <div class="col-xs-12">
@@ -46,12 +43,47 @@
     <tbody>
       <div class="search">
 
-		<form action=" "/>
-		<input type="text" name="search" placeholer="Enter Keyword Here..."/>
-		<input type="submit" value="Search Here"/>
+		<form action="search.php?searching=true" method="POST">
+		<input type="text" name="searchcategory" value=""/>
+		<input type="submit" value="Search"/>
 		</form>
 	</div>
-
+<table border="3" width="450" height ="200" cellpadding="10" cellspacing="10">
+	<?php
+	if('searching'==true && isset($_REQUEST['searchcategory'])){
+		
+	require("databaseconnect.php");
+	$sql=$db->prepare('SELECT * FROM item WHERE ITEM_DEPARTMENT=:category');
+    
+		$sql->execute(array(':category'=> $_REQUEST['searchcategory']));
+		echo "<th><font size='4'><em>Name</em></font></th>
+			<th><em><font size='4'>Brand</em></font></th>
+			<th><em><font size='4'>Color</em></font></th>
+			<th><em><font size='4'>Department</em></font></th>
+			<th><em><font size='4'>Price</em></font></th>
+			<th><em><font size='4'>Action</em></font></th>";
+		
+	
+	?>
+	<?php
+    while ($row=$sql->fetch())
+    {
+		?>
+       
+       <tr>
+				<td><font size='4'><?php echo $row['ITEM_NAME']?></font></td>
+				<td><font size='4'><?php echo $row['ITEM_BRAND']?></font></td>
+				<td><font size='4'><?php echo $row['ITEM_COLOR']?></font></td>
+				<td><font size='4'><?php echo $row['ITEM_DEPARTMENT']?></font></td>
+				<td><font size='4'><?php echo $row['ITEM_PRICE']?></font></td></td>
+				<td><a href="search.php?page=search&action=add&id=<?php echo $row['ITEM_NUMBER'] ?>"><font size='4'>Add to cart</font></a></td>
+			</tr>
+    <?php
+	}
+	}
+	?>
+	
+</table>
 
 </div></div>
 </div>
